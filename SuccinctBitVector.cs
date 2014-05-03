afec78578107c057858652fdde173a33bd857ca7
Akira Masuda <iranai.nanimo.suteteshimaou@gmail.com>
2014-05-03 18:23:00 +0900

    Bug fix.

--- SuccinctBitVector.cs
+++ SuccinctBitVector.cs
@@ -7,10 +7,10 @@ namespace Succinct
         const int BIT_FLG_LENGTH = 25000; // range of values(should be less than max range of int which is 2,147,483,647)
         const int BIT_FLG_ARRAY_LENGTH = BIT_FLG_LENGTH / 32; // 32 = 4byte which is type long's size
         // bit-flg array which describe whether the value exists in specified index
-        static ulong[] BIT_FLG_ARRAY = new ulong[BIT_FLG_ARRAY_LENGTH];
+        ulong[] bitFlgArray = new ulong[BIT_FLG_ARRAY_LENGTH];
         // Indices which count the number of bit-flgs. Number of flgs has to be fewer than 1-65535
         // If the number of bit-flgs is lower than 256 use byte rather than ushort.
-        static ushort[] BIT_CNT_ARRAY = new ushort[BIT_FLG_ARRAY_LENGTH];
+        ushort[] bitCountArray = new ushort[BIT_FLG_ARRAY_LENGTH];
         // Squeeze all data into this list
         List<T> denseDataList = new List<T>();
 
@@ -20,32 +20,32 @@ namespace Succinct
         {
             var bitFlg = 0x1UL << (idx % 64);
             // Operate "or" to bit flg array
-            BIT_FLG_ARRAY[idx / 64] |= bitFlg;
+            bitFlgArray[idx / 64] |= bitFlg;
 
             denseDataList.Add(value);
         }
 
-        // Actually this is an ugly solution because you can just define GetValue method
+        // Actually this is an ugly solution because you can define GetValue method
         // and switch conditions depends on Types like "if(typeof(T) == typeof(int))"
-        // but I didn't want to use "if" inside these methods so I changed the name of the methods.
+        // but I didn't want to use "if" inside these methods so I changed the name of the method.
 
         // This method throws exception if type T is not numeric
         public T GetValue_Numeric(int idx)
         {
             // casting 0 to object and then cast to T
-            return isDatumExists(idx) ? denseDataList[rank(idx) - 1] : (T)(object)0;
+            return isDatumExists(idx) ? denseDataList[rank(idx) - 1] : (T)(dynamic)0;
         }
         // This method throws exception if type T is not nullable(e.g. numeric Types)
         public T GetValue_Nullable(int idx)
         {
-            return isDatumExists(idx) ? denseDataList[rank(idx) - 1] : (T)(object)null;
+            return isDatumExists(idx) ? denseDataList[rank(idx) - 1] : (T)(dynamic)null;
         }
 
         bool isDatumExists(int idx)
         {
             var bitFlg = 0x1UL << (idx % 64);
             // check whether the flg at the idx is 1
-            var flg = BIT_FLG_ARRAY[idx / 64] & bitFlg;
+            var flg = bitFlgArray[idx / 64] & bitFlg;
             return flg != 0x0UL;
         }
 
@@ -55,16 +55,16 @@ namespace Succinct
         {
             ulong bit_mask = ulong.MaxValue; // all 1
             if (idx % 64 < 63) bit_mask = (0x1UL << (idx % 64 + 1)) - 0x1UL;
-            return (ushort)(BIT_CNT_ARRAY[idx / 64] + popCount64(BIT_FLG_ARRAY[idx / 64] & bit_mask));
+            return (ushort)(bitCountArray[idx / 64] + popCount64(bitFlgArray[idx / 64] & bit_mask));
         }
 
         public void Build()
         {
             // count flags that have true values
-            BIT_CNT_ARRAY[0] = 0; // No flg is true before idx 0
+            bitCountArray[0] = 0; // No flg is true before idx 0
             for (int array_idx = 0; array_idx < BIT_FLG_ARRAY_LENGTH - 1; array_idx++)
             {
-                BIT_CNT_ARRAY[array_idx + 1] = (ushort)(BIT_CNT_ARRAY[array_idx] + popCount64(BIT_FLG_ARRAY[array_idx]));
+                bitCountArray[array_idx + 1] = (ushort)(bitCountArray[array_idx] + popCount64(bitFlgArray[array_idx]));
             }
         }
         // This method simply counts the number of true flags in 64bit(= x)
